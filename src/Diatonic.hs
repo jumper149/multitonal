01c24f9fddc39fee4d2fdc603858dd2dac4d6749
Felix Springer <felixspringer149@gmail.com>
2019-10-23 22:50:42 +0200

    nicer Scale datatype, next commit will handle exports!

--- src/Diatonic.hs
+++ src/Diatonic.hs
@@ -1,4 +1,9 @@
-module Diatonic where
+{-# LANGUAGE RecordWildCards #-}
+
+module Diatonic ( Mode (..)
+                , Scale (..)
+                , scale
+                ) where
 
 import Note
 import Interval
@@ -33,11 +38,25 @@ scaleSemisteps Mixolydian = ScaleSemisteps From3To4 From6To7
 scaleSemisteps Aeolian    = ScaleSemisteps From2To3 From5To6
 scaleSemisteps Locrian    = ScaleSemisteps From1To2 From4To5
 
-type Scale = [Tone]
+data Scale = Scale { s1 :: Tone
+                   , s2 :: Tone
+                   , s3 :: Tone
+                   , s4 :: Tone
+                   , s5 :: Tone
+                   , s6 :: Tone
+                   , s7 :: Tone
+                   }
+  deriving (Read, Eq)
+
+instance Show Scale where
+  show s = unwords $ "Scale" : (show <$> tones)
+    where tones = [ s1 s , s2 s , s3 s , s4 s , s5 s , s6 s , s7 s ]
 
 scale :: Mode -> Tone -> Scale
-scale m t = toEnum . (`mod` (fromEnum (maxBound :: Tone) + 1)) . (+ fromEnum t) . fromEnum <$> halfstepsFromRoot
-  where halfstepsFromRoot = [ sum . take n $ semisteps | n <- [ 0 .. 6 ] ]
+scale m t = Scale { .. }
+  where [s1,s2,s3,s4,s5,s6,s7] = toTone <$> halfstepsFromRoot
+        toTone = toEnum . (`mod` (fromEnum (maxBound :: Tone) + 1)) . (+ fromEnum t) . fromEnum
+        halfstepsFromRoot = [ sum . take n $ semisteps | n <- [ 0 .. 6 ] ]
         semisteps = insertSemistep trans2 . insertSemistep trans1 $ wholesteps
         wholesteps = repeat (2 :: HalfSteps)
         (ScaleSemisteps trans1 trans2) = scaleSemisteps m

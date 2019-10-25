fb09a86dc608254670aecdaeb8ccc4841c915c9f
Felix Springer <felixspringer149@gmail.com>
2019-10-26 00:12:06 +0200

    massively decreased bloat in Diatonic!

--- src/Diatonic.hs
+++ src/Diatonic.hs
@@ -8,8 +8,6 @@ module Diatonic ( Mode (..)
 import Note
 import Interval
 
-import Data.List (sort)
-
 -- | Mode of a diatonic scale.
 data Mode = Ionian
           | Dorian
@@ -20,24 +18,9 @@ data Mode = Ionian
           | Locrian
   deriving (Read, Show, Eq, Ord, Enum, Bounded)
 
-data Transition = From1To2
-                | From2To3
-                | From3To4
-                | From4To5
-                | From5To6
-                | From6To7
-                | From7To8
-  deriving (Read, Show, Eq, Ord, Enum, Bounded)
-
--- Specify semitone 'Transition's in order. Rest will be whole steps in the scale.
-data ScaleSemisteps = ScaleSemisteps Transition Transition
-
-scaleSemisteps :: Mode -> ScaleSemisteps
-scaleSemisteps m = ScaleSemisteps fstHs sndHs
-  where [ fstHs , sndHs ] = sort $ shift <$> ionianHs
-        ionianHs = [ From3To4 , From7To8 ]
-        shift = toEnum . limit . (+ (- fromEnum m)) . fromEnum
-        limit = (`mod` (fromEnum (maxBound :: Transition) + 1))
+scaleSemisteps :: Mode -> [HalfSteps]
+scaleSemisteps m = take (length ionianHs) . drop (fromEnum m) $ cycle ionianHs
+  where ionianHs = [ 2 , 2 , 1 , 2 , 2 , 2 , 1 ]
 
 -- | Diatonic scale.
 data Scale = Scale { s1 :: Note
@@ -59,11 +42,5 @@ scale :: Mode -> Note -> Scale
 scale m t = Scale { .. }
   where [s1,s2,s3,s4,s5,s6,s7] = toNote <$> halfstepsFromRoot
         toNote = toEnum . (`mod` (fromEnum (maxBound :: Note) + 1)) . (+ fromEnum t) . fromEnum
-        halfstepsFromRoot = [ sum . take n $ semisteps | n <- [ 0 .. 6 ] ]
-        semisteps = insertSemistep trans2 . insertSemistep trans1 $ wholesteps
-        wholesteps = repeat (2 :: HalfSteps)
-        (ScaleSemisteps trans1 trans2) = scaleSemisteps m
-
-insertSemistep :: Transition -> [HalfSteps] -> [HalfSteps]
-insertSemistep t hs = hd ++ 1 : tl
-  where (hd , tl) = splitAt (fromEnum t) hs
+        halfstepsFromRoot = [ sum . take n $ semisteps | n <- [ 0 .. length semisteps - 1 ] ]
+        semisteps = scaleSemisteps m

17d93e7edefa52433106bd23e3742251e1671904
Felix Springer <felixspringer149@gmail.com>
2019-10-27 19:10:32 +0100

    renaming in Frequency

--- src/Frequency.hs
+++ src/Frequency.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Frequency ( Hertz (..) -- (..) necessary?
-                 , Cent (..) -- (..) necessary?
+module Frequency ( Frequency
+                 , Cent
                  , Tuning (..)
                  , standardTuning
                  , correctCent
-                 , toHertz
-                 , fromHertz
+                 , toFrequency
+                 , fromFrequency
                  ) where
 
 import Note
@@ -14,11 +14,11 @@ import Interval
 import Ratio
 
 -- | Frequency in Hertz.
-newtype Hertz = Hertz Double
+newtype Frequency = Hertz Double
   deriving (Read, Eq, Ord, Num, Fractional, Floating, Real, RealFrac)
 
-instance Show Hertz where
-  show f = (show . (round :: Hertz -> Integer)) f ++ "Hz"
+instance Show Frequency where
+  show f = (show . (round :: Frequency -> Integer)) f ++ "Hz"
 
 -- | Hundreths of 'HalfSteps'.
 newtype Cent = Cent Double
@@ -31,18 +31,19 @@ data IntonatedTone = Intonated Tuning Tone
   deriving (Read, Show)
 
 -- | Intonation of each Tone on the chromatic scale.
-newtype Tuning = EqualTemperament Hertz -- ^ equal temperament with concert pitch given by the 'Frequency' of @A₄@
+newtype Tuning = EqualTemperament Frequency -- ^ equal temperament with concert pitch given by the 'Frequency' of @A₄@
   deriving (Read, Show, Eq)
 
 standardTuning :: Tuning
-standardTuning = EqualTemperament $ Hertz 440
+standardTuning = EqualTemperament 440
 
-toHertz :: IntonatedTone -> Hertz
-toHertz (EqualTemperament a `Intonated` t) = a * 2 ** (hs / 12)
+toFrequency :: IntonatedTone -> Frequency
+toFrequency (EqualTemperament a `Intonated` t) = a * 2 ** (hs / 12)
   where hs = fromIntegral $ fromEnum t - fromEnum (Tone 4 A)
 
-fromHertz :: Tuning -> Hertz -> (IntonatedTone , Cent)
-fromHertz (EqualTemperament a) h = (EqualTemperament a `Intonated` t , ct)
+-- | Return the closest 'IntonatedTone' and the distance to the actual 'Frequency' in 'Cent'
+fromFrequency :: Tuning -> Frequency -> (IntonatedTone , Cent)
+fromFrequency (EqualTemperament a) h = (EqualTemperament a `Intonated` t , ct)
   where t = transpose wholeHs $ Tone 4 A
         ct = Cent . (100 *) $ missingHs
         wholeHs = round hs
@@ -54,8 +55,8 @@ fromHertz (EqualTemperament a) h = (EqualTemperament a `Intonated` t , ct)
 correctCent :: Tuning -> Tone -> Tone -> Cent
 correctCent t x y = Cent $ 100 * 12 * log quotient / log 2
   where Hertz quotient = xHz / yHz
-        xHz = predictHertz t (interval x y) x
-        yHz = toHertz . Intonated t $ y
+        xHz = predictFrequency t (interval x y) x
+        yHz = toFrequency . Intonated t $ y
 
-predictHertz :: Tuning -> Interval -> Tone -> Hertz
-predictHertz t i x = (* (toHertz . Intonated t) x) . fromRational . fromInterval $ i
+predictFrequency :: Tuning -> Interval -> Tone -> Frequency
+predictFrequency t i x = (* (toFrequency . Intonated t) x) . fromRational . fromInterval $ i

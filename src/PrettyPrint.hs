88cc7dbc187596e769ef258930161fc723303976
Felix Springer <felixspringer149@gmail.com>
2020-04-03 20:02:10 +0200

    use default instance for PrettyPrint

--- src/PrettyPrint.hs
+++ src/PrettyPrint.hs
@@ -7,7 +7,15 @@ import Data.Foldable (toList)
 
 -- | A class for pretty-printing.
 class PrettyPrint p where
-    pp :: p -> String
+  pp :: p -> String
 
-instance (Foldable f, Functor f, PrettyPrint p) => PrettyPrint (f p) where
-    pp fp = unlines . toList $ pp <$> fp
+instance (PrettyPrint p1, PrettyPrint p2) => PrettyPrint (p1,p2) where
+  pp (p1 , p2) = pp p1 <> ", " <> pp p2
+
+instance PrettyPrint p => PrettyPrint [p] where
+  pp ls = unlines $ pp <$> ls
+
+newtype PrettyDefault a = PrettyDefault a
+
+instance (Foldable f, Functor f, PrettyPrint p) => PrettyPrint (PrettyDefault (f p)) where
+  pp (PrettyDefault fp) = unlines . toList $ pp <$> fp
